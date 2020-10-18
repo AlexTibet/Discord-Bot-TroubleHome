8bb2cef5db4292d30e17e8ac7ddbba3e25cd09e8
Alexandr Sobko <alexfreetibet@gmail.com>
2020-10-18 23:51:26 +0700

    Добавлено - недостающее описание функций Удалено - принты

--- finde_and_download.py
+++ finde_and_download.py
@@ -5,46 +5,39 @@ from config import \
 import datetime
 import json
 from os import path
-import config
 
 
 async def download_server_log() -> bool:
+    """Скачиваем  файл текущих логов игрового сервера (основного)"""
     with FTP() as ftp:
         ftp.connect(main_host, main_port)
         ftp.login(main_login, main_password)
-        print(ftp)
-        print(ftp.retrlines('LIST'))
         ftp.cwd(main_logs_directory)
-        print(ftp.retrlines('LIST'))
         try:
             with open('download_logs.log', 'wb') as f:
                 ftp.retrbinary('RETR ' + 'BeastsOfBermuda.log', f.write)
                 return True
         except FileNotFoundError:
-            print("File BeastsOfBermuda.log not found!")
             return False
 
 
 async def download_server_config(server: tuple) -> bool:
+    """Скачиваем  конфигурационный файл с игрового сервера server"""
     bob_host, bob_port, bob_login, bob_password, bob_directory = server
     with FTP() as ftp:
         ftp.connect(bob_host, bob_port)
         ftp.login(bob_login, bob_password)
-        print(ftp)
-        print(ftp.retrlines('LIST'))
         ftp.cwd(bob_directory)
-        print(ftp.retrlines('LIST'))
         try:
             with open('download_Game.ini', 'wb') as f:
                 ftp.retrbinary('RETR ' + 'Game.ini', f.write)
                 return True
         except FileNotFoundError:
-            print("File Game.ini not found!")
             return False
 
 
 async def upload_server_config(server: tuple) -> bool:
-    """Загружает обновлённый конфиг на сервер"""
+    """Загружаем обновлённый конфигурационный файл на сервер server"""
     try:
         bob_host, bob_port, bob_login, bob_password, bob_directory = server
         with FTP() as ftp:
@@ -52,25 +45,18 @@ async def upload_server_config(server: tuple) -> bool:
             ftp.login(bob_login, bob_password)
             ftp.cwd(bob_directory)
             ftp.storbinary('STOR ' + "Game.ini", open('Game.ini', "rb"))
-            print("Файл загружен на сервер")
             return True
-    except Exception as error:
-        print("Ошибка загрузки", error, error.__doc__, error.__module__)
+    except Exception:
         return False
 
 
 def download_server_saves(server: tuple) -> bool:
+    """Скачиваем сохранения игрового сервера server"""
     host, port, login, password, directory = server
-    print(host, port)
-    print(login, password)
-    print(directory)
     with FTP() as ftp:
         ftp.connect(host, port)
         ftp.login(login, password)
-        print(ftp)
-        print(ftp.retrlines('LIST'))
         ftp.cwd(directory)
-        print(ftp.retrlines('LIST'))
         try:
             with open(f'{path.dirname(__file__)}/server_saves/DeathHistory.sav', 'wb') as f:
                 ftp.retrbinary('RETR ' + 'SERVER_Rival_Shores_DeathHistory.sav', f.write)
@@ -92,20 +78,17 @@ def download_server_saves(server: tuple) -> bool:
 
             return True
         except FileNotFoundError:
-            print("File not found!")
             return False
 
 
 def upload_server_saves(server: tuple) -> bool:
-    """Загружает обновлённый конфиг на сервер"""
+    """Загружаем сохранения на игровой сервер server"""
     try:
         bob_host, bob_port, bob_login, bob_password, bob_directory = server
         with FTP() as ftp:
             ftp.connect(bob_host, bob_port)
             ftp.login(bob_login, bob_password)
-            print(ftp.retrlines('LIST'))
             ftp.cwd(bob_directory)
-            print(ftp.retrlines('LIST'))
             ftp.storbinary(
                 'STOR ' + 'SERVER_Rival_Shores_DeathHistory.sav',
                 open(f'{path.dirname(__file__)}/server_saves/DeathHistory.sav', "rb")
@@ -130,7 +113,6 @@ def upload_server_saves(server: tuple) -> bool:
                 'STOR ' + 'SERVER_Rival_Shores_WorldItems.sav',
                 open(f'{path.dirname(__file__)}/server_saves/WorldItems.sav', "rb")
             )
-            print("Файл загружен на сервер")
             return True
     except Exception as error:
         print("Ошибка загрузки", error, error.__doc__, error.__module__)
@@ -149,8 +131,6 @@ async def download_log(steam_id: int) -> str:
             with open(download_filename, 'wb') as f:
                 ftp.retrbinary('RETR ' + f"{steam_id}.json", f.write)
                 return download_filename
-        else:
-            print(f"Игрока {steam_id} нет в базе данных")
 
 
 async def upload_log(filename: str, steam_id: int) -> bool:
@@ -161,10 +141,8 @@ async def upload_log(filename: str, steam_id: int) -> bool:
             ftp.login(login, password)
             ftp.cwd(directory)
             ftp.storbinary('STOR ' + f"{steam_id}.json", open(filename, "rb"))
-            print("Файл загружен на сервер")
             return True
-    except Exception as error:
-        print("Ошибка загрузки", error, error.__doc__, error.__module__)
+    except Exception:
         return False
 
 
@@ -172,11 +150,8 @@ async def data_modification(steam_id: int, new_dino: str) -> str or False:
     """Изменяет данные игрока в .json"""
     file = await download_log(steam_id)
     if file is None:
-        print(f"Игрока {steam_id} нет в базе данных")
         return False
-    print(f"Модифицируем файл {file}")
     with open(file, 'r') as read_file:
-        print(f"Файл {file} готов к модификации")
         data = json.load(read_file)
         if data is None:
             return False
@@ -187,10 +162,8 @@ async def data_modification(steam_id: int, new_dino: str) -> str or False:
         data['Thirst'] = "20000"
         data['Stamina'] = "20000"
         data['Health'] = "20000"
-        print(f"Изменения внесены")
         with open(f"{path.dirname(__file__)}/history/{steam_id}.json", "w") as output_file:
             json.dump(data, output_file, indent=4)
-            print(f"Изменённый файл ({steam_id}.json) готов к загрузке на сервер")
         return f"{path.dirname(__file__)}/history/{steam_id}.json"
 
 
